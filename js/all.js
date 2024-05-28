c96c4efd085f1cd382339946962f09c57255ac43
yanxun2474 <yanxun2474@gmail.com>
2024-05-28 23:46:38 +0800

    新增環境

---
+++ js/all.js
@@ -0,0 +1,5 @@
+$(".header-content-btn").click(function(){
+    $(".hamburger-icon").toggleClass("btn-close");
+    $(".close-icon").toggleClass("btn-active");
+    $(".header-content-menu").toggleClass("menu-active");
+});
