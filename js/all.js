dd6488597f1baa9872387e1577de3a6ace288cb7
yanxun2474 <yanxun2474@gmail.com>
2024-05-31 21:03:46 +0800

    完成首頁

--- js/all.js
+++ js/all.js
@@ -1,5 +1,9 @@
-$(".header-content-btn").click(function(){
-    $(".hamburger-icon").toggleClass("btn-close");
-    $(".close-icon").toggleClass("btn-active");
-    $(".header-content-menu").toggleClass("menu-active");
+$(document).ready(() => {
+    $(".header-content-btn").click(function(){
+        // $(".header-content-menu").slideToggle("slow");
+        // $(".header-content-menu").css('display', 'flex');
+        $(".hamburger-icon").toggleClass("btn-close");
+        $(".close-icon").toggleClass("btn-active");
+        $(".header-content-menu").toggleClass("menu-active");
+    })
 });
